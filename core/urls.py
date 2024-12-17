144944c1d117b7cc26b768d9b7617245c005a236
Eyob Abdella <eyobabdellasharo@gmail.com>
2024-12-17 22:33:42 +0500

    Debugging CI/CD

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GoogleRedirectAPI, GoogleAPI, testing
+from .views import GoogleRedirectAPI, GoogleAPI
 
 urlpatterns = [
     path("google-oauth2/callback/", GoogleAPI.as_view()),
@@ -8,5 +8,5 @@ urlpatterns = [
         GoogleRedirectAPI.as_view(),
         name="google-oauth2-redirect",
     ),
-    path("testing/", testing, name="testing"),  # for testing purposes only
+    # path("testing/", testing, name="testing"),  # for testing purposes only
 ]
