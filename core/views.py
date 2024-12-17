5cde90c2e9686526fe998dea37dc404200cbfb0c
Eyob Abdella <eyobabdellasharo@gmail.com>
2024-12-17 21:47:54 +0500

    Testing CI/CD

--- core/views.py
+++ core/views.py
@@ -83,4 +83,5 @@ class GoogleAPI(PublicAPI):
 
 from django.http import HttpResponse
 def testing(request):
+    # T Add tests here. For now, we're just returning a simple response.
     return HttpResponse("testing")
