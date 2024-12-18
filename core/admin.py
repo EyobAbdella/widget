42353672e093973a52fd8bdf24af467f81877f40
Eyob Abdella <eyobabdellasharo@gmail.com>
2024-12-18 18:49:45 +0500

    admin and setting files updated

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(GoogleSheetToken)
+admin.site.register(OAuthSession)
+admin.site.register(User)
+
--- widget/admin.py
+++ widget/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import AdminBrandInfo
+from .models import *
 
+
+admin.site.register(EmailNotification)
 admin.site.register(AdminBrandInfo)
+admin.site.register(UserBrandInfo)
+admin.site.register(WidgetData)
+admin.site.register(WidgetFile)
+admin.site.register(SubmittedData)
+admin.site.register(PreFill)
+admin.site.register(SubmitButton)
+admin.site.register(FormTemplate)
+
+
