1a7620b4b5cb92a0ad83e5dc51ceeef39d1c6327
Eyob Abdella <eyobabdellasharo@gmail.com>
2025-01-03 15:48:24 +0500

    Update endpoint url

--- static/platform.js
+++ static/platform.js
@@ -1,7 +1,7 @@
 (function () {
   const staticFileEndpoint = "https://widgetcontact.myfindata.com";
   const RECAPTCHA_SITE_KEY = "6LdL15YqAAAAAJADkgf9Nq9NGS88QA2WFcRtzWmu";
-  const endpointUrl = "https://widgetcontact.myfindata.com/contact-form";
+  const endpointUrl = "https://widgetcontact.myfindata.com/widgets/form";
   const link = document.createElement("link");
   link.rel = "stylesheet";
   link.href = `${staticFileEndpoint}/static/style.css`;
